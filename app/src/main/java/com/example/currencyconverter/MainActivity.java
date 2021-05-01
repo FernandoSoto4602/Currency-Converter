8b8ebd698be77d8a0f4a056f94ec4ca8772af4e0
Fernando Soto <52804330+FernandoSoto4602@users.noreply.github.com>
2021-05-01 08:05:30 -0500

    Added comments

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -21,18 +21,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void convertMoney(View view) {
+        // Grabs the string from the usdTextBox and stores it into moneyText
         EditText moneyText = (EditText) (findViewById(R.id.usdTextBox));
 
+        // Checks to make sure we have some type of input inside the textField and we also call the isNumeric function to make sure the inputted String is numeric only else we set error to null
+        // We display a Toast bubble to let the user know they need to input a amount to convert in USD until a valid input is entered
         if(moneyText.getText().toString().length() == 0 || !isNumeric(moneyText.getText().toString())){
             moneyText.setError(null);
             Toast.makeText(getApplicationContext(), "Please enter USD amount ", Toast.LENGTH_SHORT).show();
         } else{
+            // Using the created spinner we get the selected option the user has picked to help convert to the desired currency
             Spinner spinner = (Spinner) findViewById(R.id.currencySpinner);
             String selectedCurrency = spinner.getSelectedItem().toString();
 
-
+            // We get the String from moneyText and convert it to a Double
             double moneyDouble = Double.parseDouble(moneyText.getText().toString());
+            // To find the final result we call the convertCurrency with the spinner selection and amount that was converted
             double result = convertCurrency(moneyDouble, selectedCurrency);
+            // Convert the Double we got back from convertCurrency back to a String to use it with Toast
             String stringResult = Double.toString(result);
             Toast.makeText(getApplicationContext(), stringResult, Toast.LENGTH_SHORT).show();
         }
@@ -40,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public double convertCurrency(double amount, String currency) {
+        // Creates a Double named result and converts the currency selected
         double result;
 
         if (currency.equals("EUR")) {
@@ -65,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    // Function to check whether a String is numeric
     public static boolean isNumeric(String strNum) {
         if (strNum == null) {
             return false;
