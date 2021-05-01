fa440a199a6bff8339d96039c342e9c18c82c2c1
Fernando Soto <52804330+FernandoSoto4602@users.noreply.github.com>
2021-05-01 07:33:18 -0500

    Added Spinner with multiple options to select different currencies

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,21 +16,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
-    public void convertMoney(View view){
+    public void convertMoney(View view) {
+
+        Spinner spinner = (Spinner) findViewById(R.id.currencySpinner);
+        String selectedCurrency = spinner.getSelectedItem().toString();
+
         EditText moneyText = (EditText) (findViewById(R.id.usdTextBox));
         double moneyDouble = Double.parseDouble(moneyText.getText().toString());
-        double result = convertCurrency(moneyDouble);
+        double result = convertCurrency(moneyDouble, selectedCurrency);
         String stringResult = Double.toString(result);
-        Toast.makeText(getApplicationContext(),stringResult,Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), stringResult, Toast.LENGTH_SHORT).show();
+
     }
 
-    public double convertCurrency(double amount){
-        double euroAmount =  .83;
-        double finalAmount;
-        finalAmount = amount * euroAmount;
+    public double convertCurrency(double amount, String currency) {
+        double result;
 
-        return finalAmount;
+        if (currency.equals("EUR")) {
+            result = amount * .85;
+            return result;
+        } else if (currency.equals("JPY")) {
+            result = amount * 109.30;
+            return result;
+        } else if (currency.equals("GBP")) {
+            result = amount * .72;
+            return result;
+        } else if (currency.equals("CHF")) {
+            result = amount * .91;
+            return result;
+        } else if (currency.equals("CAD")) {
+            result = amount * 1.23;
+            return result;
+        } else if (currency.equals("ZAR")) {
+            result = amount * 14.50;
+            return result;
+        } else
+            return 0.00;
+
     }
 }
